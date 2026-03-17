f91551838255133fda7c541340d842bc9fc270a3
Tsinswreng <Tsinswreng@gmail.com>
2026-03-17 18:09:04 +0800

    緟構IPropAccessor

--- proj/Tsinswreng.CsStrAcc/IAccessor.cs
+++ proj/Tsinswreng.CsStrAcc/IAccessor.cs
@@ -3,34 +3,31 @@ using System.Collections.Generic;
 using Tsinswreng.CsCore;
 namespace Tsinswreng.CsStrAcc;
 
-[Doc(@$"#See[{nameof(IPropAccessor)}]")]
-public interface IPropAccessorForOneType{
-	public Type TargetType{get;}
-	public bool TryGet(obj? O, str Key, out obj? R);
-	[Doc(@$"
-	#Rtn[true if ok]")]
-	public bool TrySet(obj? O, str Key, obj? Value);
-	public IReadOnlyList<str> GetProps(obj? O, OptGetProps? Opt = null);
-	public bool TryGetType(str Key, out Type? Type);
-}
 
 [Doc(@$"Access props by string
 only support props(getter, setter, etc.)
 functions are not included currently because they may have overloads
 ")]
 public interface IPropAccessor{
-	public bool TryGet(obj? O, Type Target, str Key, out obj? R);
+	public Type TargetType{get;}
 	[Doc(@$"
+	#Params([its type should be assignable to `{nameof(TargetType)}`])
+	")]
+	public bool TryGet(obj? O, str Key, out obj? R);
+	[Doc(@$"
+	#Params([its type should be assignable to `{nameof(TargetType)}`])
 	#Rtn[true if ok]")]
-	public bool TrySet(obj? O, Type Target, str Key, obj? Value);
-	
+	public bool TrySet(obj? O, str Key, obj? Value);
 	[Doc(@$"
-	get properties of object(getter, setter etc)
-	raw fields, methods are not included
-	#Params([],[Option, not supported yet, reserved for future use])
-	#Rtn[Read only, thus in every call, address of returned list may not change ]")]
-	public IReadOnlyList<str> GetProps(Type Target, OptGetProps? Opt = null);
-	[Doc(@$"Get Declared type of prop
+	#Params(
+	[its type should be assignable to `{nameof(TargetType)}`],
+	[Option, not supported yet, reserved for future use],
+	)
+	#Rtn[Read only, thus in every call, address of returned list may not change ]
+	")]
+	public IReadOnlyList<str> GetPropNames(obj? O, OptGetPropNames? Opt = null);
+	[Doc(@$"
+	Get Declared type of {nameof(TargetType)}'s Member at {nameof(Key)}
 	NOT the same as below:
 	```cs
 	{nameof(TryGet)}(o, key, out var v);
@@ -39,15 +36,27 @@ public interface IPropAccessor{
 	+ out var v may be null, if null, you won't get the type
 	+ GetType() returns actual type, not declared type
 	")]
+	
+	public bool TryGetType(str Key, out Type? Type);
+}
+
+[Doc(@$"no content, reserved for future use")]
+public class OptGetPropNames{
+	
+}
+
+
+public interface IPropAccessorMgr{
+	public IDictionary<Type, IPropAccessor> Type_PropAccessor{get;set;}
+	#region 下面這些放到IPropAccessorMgr的擴展方法裏去
+	public bool TryGet(obj? O, Type Target, str Key, out obj? R);
+
+	public bool TrySet(obj? O, Type Target, str Key, obj? Value);
+	
+	public IReadOnlyList<str> GetProps(Type Target, OptGetPropNames? Opt = null);
+	
 	public bool TryGetType(Type Target, str Key, out Type? Type);
-}
-
-[Doc(@$"no content, reserved for future use")]
-public class OptGetProps{
+	#endregion
 	
 }
 
-[Doc(@$"no content, reserved for future use")]
-public class OptGetKeys{
-	
-}
