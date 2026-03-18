7bbc522bbce94d68e5c308a1b360af7520fc339c
Tsinswreng <Tsinswreng@gmail.com>
2026-03-18 16:23:32 +0800

    wip:PropDict

--- proj/Tsinswreng.CsStrAcc/PropDict.cs
+++ proj/Tsinswreng.CsStrAcc/PropDict.cs
@@ -1,61 +1,68 @@
-//TODO
-//public class PropDict : IDictionary<str, obj?> {
-// 	IPropAccessor PropAcc;
-// 	Type TargetType;
-// 	obj? TargetObj;
-// 	public object? this[string key] {
-// 		get=>
-// 	}
+using Tsinswreng.CsCore;
 
-// 	public ICollection<string> Keys => throw new NotImplementedException();
+namespace Tsinswreng.CsStrAcc;
+public interface IPropDict:IDictionary<str, obj?>{
+	public IPropAccessor PropAccessor{get;set;}
+	public Type TargetType{get;set;}
+	
+	[Doc(@$"Should be assignable to {nameof(TargetType)}")]
+	public obj? TargetObj{get;set;}
+}
+public class PropDict : IPropDict {
+	
+	public object? this[string key] {
+		get=>
+	}
 
-// 	public ICollection<object?> Values => throw new NotImplementedException();
+	public ICollection<string> Keys => throw new NotImplementedException();
 
-// 	public int Count => throw new NotImplementedException();
+	public ICollection<object?> Values => throw new NotImplementedException();
 
-// 	public bool IsReadOnly => throw new NotImplementedException();
+	public int Count => throw new NotImplementedException();
 
-// 	public void Add(string key, object? value) {
-// 		throw new NotImplementedException();
-// 	}
+	public bool IsReadOnly => throw new NotImplementedException();
 
-// 	public void Add(KeyValuePair<string, object?> item) {
-// 		throw new NotImplementedException();
-// 	}
+	public void Add(string key, object? value) {
+		throw new NotImplementedException();
+	}
 
-// 	public void Clear() {
-// 		throw new NotImplementedException();
-// 	}
+	public void Add(KeyValuePair<string, object?> item) {
+		throw new NotImplementedException();
+	}
 
-// 	public bool Contains(KeyValuePair<string, object?> item) {
-// 		throw new NotImplementedException();
-// 	}
+	public void Clear() {
+		throw new NotImplementedException();
+	}
 
-// 	public bool ContainsKey(string key) {
-// 		throw new NotImplementedException();
-// 	}
+	public bool Contains(KeyValuePair<string, object?> item) {
+		throw new NotImplementedException();
+	}
 
-// 	public void CopyTo(KeyValuePair<string, object?>[] array, int arrayIndex) {
-// 		throw new NotImplementedException();
-// 	}
+	public bool ContainsKey(string key) {
+		throw new NotImplementedException();
+	}
 
-// 	public IEnumerator<KeyValuePair<string, object?>> GetEnumerator() {
-// 		throw new NotImplementedException();
-// 	}
+	public void CopyTo(KeyValuePair<string, object?>[] array, int arrayIndex) {
+		throw new NotImplementedException();
+	}
 
-// 	public bool Remove(string key) {
-// 		throw new NotImplementedException();
-// 	}
+	public IEnumerator<KeyValuePair<string, object?>> GetEnumerator() {
+		throw new NotImplementedException();
+	}
 
-// 	public bool Remove(KeyValuePair<string, object?> item) {
-// 		throw new NotImplementedException();
-// 	}
+	public bool Remove(string key) {
+		throw new NotImplementedException();
+	}
 
-// 	public bool TryGetValue(string key, out object? value) {
-// 		throw new NotImplementedException();
-// 	}
+	public bool Remove(KeyValuePair<string, object?> item) {
+		throw new NotImplementedException();
+	}
 
-// 	IEnumerator IEnumerable.GetEnumerator() {
-// 		return GetEnumerator();
-// 	}
-// }
+	public bool TryGetValue(string key, out object? value) {
+		throw new NotImplementedException();
+	}
+
+	IEnumerator IEnumerable.GetEnumerator() {
+		return GetEnumerator();
+	}
+}
